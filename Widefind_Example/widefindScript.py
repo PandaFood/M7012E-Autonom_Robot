90a49ed9bf021c106d8cd68d0b599626fbb4795b
PandaFood <Jonathan.Brorsson@gmail.com>
2020-02-07 13:34:14 +0100

    fixed widefind for rethinkdb

--- Widefind_Example/widefindScript.py
+++ Widefind_Example/widefindScript.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import PythonRESTClient as RESTClient
+from rethinkdb import RethinkDB
 import datetime
 import time
 from datetime import datetime,timezone, timedelta, date
@@ -10,77 +10,59 @@ import json
 broker_url = "130.240.114.24"
 broker_port = 1883
 
-#out_data_path = "../data/weaklabels2/"
-out_data_path="./"
-
-#userslist = ['472491134@qq.com']#,' sy17@mails.tsinghua.edu.cn']#['moto@test.co.uk','xperia@test.co.uk']
-blacklist = ['h2al']#NO DATA EXPECTED FOR ADMIN THEREFORE IS IN BLACKLIST (no data downloaded for users in blacklist)
-entrypoint = "b4401.lab.ltu.se"
-port = "8080"
-#username = "472491134@qq.com" #
-#password = "2016213647" #
-#USING AN ADMIN ACCOUNT TO DOWNLOAD DATA
-username = "DoorSensor@gmail.com"
-password = "Door2019"
-
-aggregator = RESTClient.PythonRESTClient(entrypoint,username,password,port)
-
+#RETHINKDB
+r = RethinkDB()
+rethink_url = "localhost"
+rethink_port = 28015
 
 def refreshToken(rc):
 	rc.login()
 	return rc.token
 
-print("Logging in...")
-response_status, response_body = aggregator.login()
-
-token = aggregator.token
-
-print("Token: ", aggregator.token)
-
-user_id =  response_body["id"]
-user_name = response_body["personalData"]["userName"]
 
 #DATA must be sent inside an event object 
 event = {}
+pos = {}
 
-
-#for raw data measurement use the data field oin the event
-#in this example every event contains two 
-#event["data"] = aggregator.encode_data("x1,y1,z1,t1\nx2,y2,z2,t2\nx3,y3,z3,t3\n")
-
+ids = []
 
 
 def on_connect(client, userdata, flags, rc):
    print("Connected With Result Code "+rc)
 
 def on_message(client, userdata, message):
-   #print("Message Recieved: "+message.payload.decode())
    mqttMsgString = message.payload.decode()
    mqttMsgJson = json.loads(mqttMsgString)
-   #if (mqttMsgJson["message"][0:6] == "REPORT"):
-   print(mqttMsgJson)
+
+
+   # BEACON:96E9E196C540FE15,0.2.7,0,-4700,2700,4.00,-87.5,2051917,MAN,SAT*5B73
    mqttListIndex = mqttMsgJson["message"].split(',')
-   mqttXVar = mqttListIndex[2]
-   mqttYVar = mqttListIndex[3]
-   mqttZVar = mqttListIndex[4]
+
+   pos["X"] = mqttListIndex[2]
+   pos["Y"] = mqttListIndex[3]
+   pos["Z"] = mqttListIndex[4]
+
    timeVar= datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-   event["startTime"] = timeVar+".000+0100"
-   event["endTime"] = timeVar+".000+0100"
+
+
+   event["id"] = mqttListIndex[0][7:]
+   event["version"] = mqttListIndex[1]
+   event["time"] = mqttMsgJson["time"]
    event["type"] = "WideFind"
-   if ((int(mqttXVar) < 3150) and (int(mqttYVar) < -2000)):
-      event["label"] = "Kitchen"
+   event["pos"] = pos
+
+   msg = ""
+
+   if(str(event["id"]) not in ids):
+      msg = r.table('current_state').insert(event).run(conn)
+      ids.append(str(event["id"]))
    else:
-      event["label"] = "Livingroom"
+      msg = r.table('current_state').update(event).run(conn)
 
 
-      # All events must be sent as a list of evevnts
-      # in case of a single event you add only an event to the list
-   events = {"events":[]}
-   events["events"].append(event)
+   #print(event)
+   #print( msg )
 
-      #performing PUSH data query
-#      aggregator.addEvents(user_id,events)
-#   time.sleep(5)
 
 
 
@@ -88,10 +70,23 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker_url, broker_port)
+
+msg = {
+   'host': 'WFGATEWAY-3ABFF8D01EFF', 
+   'message': 'BEACON:96E9E196C540FE15,0.2.7,0,-4700,2700,4.00,-87.3,1183981,MAN,SAT*9383', 
+   'source': '03FF5C0A2BFA3A9B', 
+   'time': '2020-02-07T11:33:12.854889928Z', 
+   'type': 'widefind_message'
+   }
+
+conn = r.connect(host=rethink_url, port=rethink_port, db="WIDEFIND" ).repl()
+#r.db("WIDEFIND").table_create("current_state").run()
+
+
 client.loop_start()
 
 # '#' means subcribe to all topics
 client.subscribe("#")
 
 while True:
-    time.sleep(1)
+    time.sleep(2)
